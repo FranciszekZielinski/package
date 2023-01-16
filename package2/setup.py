b032d61b37e1e1d7c6923d979f55263077af51ba
Grzegorz Bokota <bokota+github@gmail.com>
2023-01-16 17:34:20 +0100

    use setup.cfg in package2

--- package2/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="simple-package-2",
-    version="0.1",
-    py_modules=["simple_package2"] # use when you would like to include modules to python package
-)
